650771570b87745d2a1cddbf221ef7dcc5f7d873
euath0ry <euath0ry@gmail.com>
2026-03-19 18:29:19 +0300

    перенес дб в новый файл

---
+++ db.py
@@ -0,0 +1,17 @@
+from models import Weather
+
+weather_db = [
+    Weather(
+        city="Москва",
+        temperature=20.5,
+        condition="облачно",
+        humidity=65
+    ),
+    Weather(
+        city="Санкт-Петербург", 
+        temperature=18.0,
+        condition="дождливо",
+        humidity=80
+    )
+]
+
